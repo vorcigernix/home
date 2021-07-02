48f5af231d5af445a593d90d713650ce20f42b35
Adam Sobotka <vorcigernix@gmail.com>
2021-07-02 13:24:48 +0200

    first

---
+++ components/cover-image.tsx
@@ -0,0 +1,33 @@
+import cn from 'classnames'
+import Link from 'next/link'
+
+type Props = {
+  title: string
+  src: string
+  slug?: string
+}
+
+const CoverImage = ({ title, src, slug }: Props) => {
+  const image = (
+    <img
+      src={src}
+      alt={`Cover Image for ${title}`}
+      className={cn('shadow-small w-full h-96 object-cover rounded-xl filter grayscale', {
+        'hover:shadow-medium transition filter grayscale-0 duration-200': slug,
+      })}
+    />
+  )
+  return (
+    <div className="sm:mx-0">
+      {slug ? (
+        <Link as={`/posts/${slug}`} href="/posts/[slug]">
+          <a aria-label={title}>{image}</a>
+        </Link>
+      ) : (
+        image
+      )}
+    </div>
+  )
+}
+
+export default CoverImage
---
+++ components/intro.tsx
@@ -0,0 +1,13 @@
+
+
+const Intro = () => {
+  return (
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8 bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 bg-clip-text text-transparent">
+        Interplanetary Blog.
+      </h1>
+    </section>
+  )
+}
+
+export default Intro
